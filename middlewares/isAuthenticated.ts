ab54d28affab332077d6fd961ad19c9f2cc8a33b
Makrand Gurav <makrand.gurav@zymr.com>
2024-05-24 12:01:29 +0530

    jwt token geenration

--- middlewares/isAuthenticated.ts
+++ middlewares/isAuthenticated.ts
@@ -1,6 +1,13 @@
 import { NextFunction } from "express";
 import { UserModel } from "../models/users.model";
 import * as bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
+const secret = 'makrandandsiddhi';
+
+const { sign, verify } = jwt
+
+
+
 export const isAuthenticated = async (req: any, res: any, next: any) => {
   console.log("Checking if user is Authenticated");
   try {
@@ -9,7 +16,7 @@ export const isAuthenticated = async (req: any, res: any, next: any) => {
 
       return res.status(400).render("login");
     }
-    const { username, userpwd } = req.body;
+    const { username, userpwd, fullname } = req.body;
     const hash: any = await UserModel.findOne({
       username,
     }).select("password -_id");
@@ -20,8 +27,35 @@ export const isAuthenticated = async (req: any, res: any, next: any) => {
       console.log("Unauthenticated");
       return res.status(401).render("login");
     }
+    const jwtToken = await generateToken({username, fullname}, secret);
+
+    if (!jwtToken) {
+      return res.status(401).render("login");
+    }
+
+    console.log(jwtToken, ':---jwtToken---');
+    res.cookie('token', jwtToken, { httpOnly: true });
     next();
   } catch (error) {
     console.log(error);
+    return res.status(401).render("login");
   }
 };
+
+export const generateToken = async (payload: Payload, jwtSecret :string ) => {
+  try {
+    const jwtOptions: any = {
+      expiresIn: '7d',
+      algorithm: 'HS384'
+    }
+    return await jwt.sign(payload, jwtSecret, jwtOptions)
+  } catch (error) {
+    throw error;
+  }
+  
+}
+
+interface Payload {
+  username: string,
+  fullname: string
+}
